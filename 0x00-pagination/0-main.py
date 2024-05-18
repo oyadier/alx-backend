753df8843e17ba658358e33a81f930c83f1aabf0
Oyadier Robert Mensah <oyadierrobertmensah@gmail.com>
2024-05-18 19:06:33 +0000

    Simple Helper function

---
+++ 0x00-pagination/0-main.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+"""
+Main file
+"""
+
+index_range = __import__('0-simple_helper_function').index_range
+
+res = index_range(1, 7)
+print(type(res))
+print(res)
+
+res = index_range(page=3, page_size=15)
+print(type(res))
+print(res)
+
+res = index_range(page=2, page_size=8)
+print(type(res))
+print(res)
---
+++ 0x00-pagination/0-simple_helper_function.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+from typing import Tuple
+""""""
+
+
+def index_range(page: int, page_size: int) -> Tuple:
+
+    """
+    A funtion to compute pagenation based of the args
+        Arg:
+            page (int) page number
+            page_size (Int) number of rows
+    """
+    start_index = (page - 1) * page_size + 1
+    end = start_index + page_size - 1
+    start_index -= 1
+    return (start_index, end)
