892a2480274c11486d980c8f5ded2d235acb7ae7
Oyadier Robert Mensah <oyadierrobertmensah@gmail.com>
2024-05-18 19:07:52 +0000

    Simple Helper function

--- 0x00-pagination/0-simple_helper_function.py
+++ 0x00-pagination/0-simple_helper_function.py
@@ -1,12 +1,11 @@
 #!/usr/bin/env python3
 from typing import Tuple
-""""""
+"""Compute pagnation"""
 
 
 def index_range(page: int, page_size: int) -> Tuple:
 
-    """
-    A funtion to compute pagenation based of the args
+    """A funtion to compute pagenation based of the args
         Arg:
             page (int) page number
             page_size (Int) number of rows
