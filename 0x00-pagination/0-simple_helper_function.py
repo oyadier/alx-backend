3b73a99c001490b120797de33a7323b1c78b6c57
Oyadier Robert Mensah <oyadierrobertmensah@gmail.com>
2024-05-18 22:52:17 +0000

    simple pagination

--- 0x00-pagination/0-simple_helper_function.py
+++ 0x00-pagination/0-simple_helper_function.py
@@ -10,6 +10,7 @@ def index_range(page: int, page_size: int) -> Tuple:
             page (int) page number
             page_size (Int) number of rows
     """
-    start_index = (page - 1) * page_size
+    start_index = (page - 1) * page_size + 1
     end = start_index + page_size - 1
+    start_index -= 1
     return (start_index, end)
