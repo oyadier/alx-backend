5d00d1b03934818400e0fea9539abcdf673d927a
Oyadier Robert Mensah <oyadierrobertmensah@gmail.com>
2024-05-18 22:43:58 +0000

    simple pagination

---
+++ 0x00-pagination/1-simple_pagination.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python3
+import csv
+import math
+from typing import List
+
+from typing import Tuple
+"""Compute pagnation"""
+
+
+def index_range(page: int, page_size: int) -> Tuple:
+
+    """A funtion to compute pagenation based of the args
+        Arg:
+            page (int) page number
+            page_size (Int) number of rows
+    """
+    start_index = (page - 1) * page_size + 1
+    end = start_index + page_size - 1
+    start_index -= 1
+    return (start_index, end)
+
+
+class Server:
+    """Server class to paginate a database of popular baby names.
+    """
+    DATA_FILE = "Popular_Baby_Names.csv"
+
+    def __init__(self):
+        self.__dataset = None
+
+    def dataset(self) -> List[List]:
+        """Cached dataset
+        """
+        if self.__dataset is None:
+            with open(self.DATA_FILE) as f:
+                reader = csv.reader(f)
+                dataset = [row for row in reader]
+            self.__dataset = dataset[1:]
+
+        return self.__dataset
+
+    def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
+        assert isinstance(page, int) and page > 0
+        assert isinstance(page_size, int) and page_size > 0
+
+        index = index_range(page, page_size)
+        sets = []
+
+        for i in index:
+            if i > len(self.dataset()):
+                return []
+            sets.append(self.dataset()[i])
+        return sets
