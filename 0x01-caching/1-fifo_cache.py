6f1e8f8dfc3194f901989d17ecbd2b1efd301619
Oyadier Robert Mensah <oyadierrobertmensah@gmail.com>
2024-05-23 04:58:41 +0000

    FIFO Caching

--- 0x01-caching/1-fifo_cache.py
+++ 0x01-caching/1-fifo_cache.py
@@ -1,39 +1,44 @@
-#!/usr/bin/python3
-"""FiFO Caching"""
+#!/usr/bin/env python3
+"""Create a class FIFOCache that inherits from
+BaseCaching and is a caching system:
+"""
 
 
-from base_caching import BaseCaching
+BaseCaching = __import__('base_caching').BaseCaching
 
 
 class FIFOCache(BaseCaching):
-    """Implement FIFO caching"""
+    """_summary_
+    """
 
     def __init__(self):
-        """Initialice the obj"""
+        """_summary_
+        """
         super().__init__()
-        self.cache_data
 
     def put(self, key, item):
-        """Add item to FIFO block
+        """_summary_
+
         Args:
-                key(integer): key of the item block
-                item(str): item of the block
+                        key (_type_): _description_
+                        item (_type_): _description_
         """
         if key is None or item is None:
             pass
-        if len(self.cache_data) >= BaseCaching.MAX_ITEMS \
-                and key not in self.cache_data.keys():
-            key, val = next(iter(self.cache_data.items()))
-            print("DISCARD: {}".format(key))
-            del self.cache_data[key]
-        self.cache_data[key] = item
+        else:
+            if len(self.cache_data) >= BaseCaching.MAX_ITEMS \
+                    and key not in self.cache_data.keys():
+                first_key = next(iter(self.cache_data.keys()))
+                del self.cache_data[first_key]
+                print("DISCARD: {}". format(first_key))
+
+            self.cache_data[key] = item
 
     def get(self, key):
-        """Get the value given the key
+        """return the value in self.cache_data linked to key
+
         Args:
-                key(integer): Value fo the item
-        Retrun:
-                item(intefer): item linked to key
+                        key (_type_): _description_
         """
         if key is None or key not in self.cache_data.keys():
             return None
