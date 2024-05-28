158226cd01e60391e199078f4ef96469d5671e47
Oyadier Robert Mensah <oyadierrobertmensah@gmail.com>
2024-05-28 16:45:50 +0000

    Basic Flask app

---
+++ 0x02-i18n/0-app.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""Flask Babel project"""
+from flask import (Flask, render_template)
+
+
+app = Flask('app', __name__)
+
+
+@app.route("/")
+def app_title():
+    return render_template('index.html')
+
+
+if __name__ == "__main__":
+    app.run(host='127.0.0.0', port=5000, debug=True)
---
+++ 0x02-i18n/README.md
@@ -0,0 +1,4 @@
+<h1>0x02. i18n</h1>
+
+1. <a href="https://blog.miguelgrinberg.com/post/the-flask-mega-tutorial-part-xiii-i18n-and-l10n">Flask i18n tutorial</a>
+2. <a href="https://pypi.org/project/pytz/">Pytz</a>
