75a355624b4dfc9e0ea14d2bb1f857a4379fc0fd
Oyadier Robert Mensah <oyadierrobertmensah@gmail.com>
2024-08-29 02:44:59 +0100

    Create Process for Job execution

--- 0x03-queuing_system_in_js/6-job_creator.js
+++ 0x03-queuing_system_in_js/6-job_creator.js
@@ -1,15 +1,16 @@
 var kue = require('kue');
-var push_notification_code = kue.createQueue();
+var queue = kue.createQueue();
 
 var jobData = {
     phoneNumber: '04343334433',
     message: 'This is the code to verify your account',
 }
 
-push_notification_code.create('push notification', jobData)
-    .save((err) => {
-        if (!err) console.log(`Notification job created: ${push_notification_code.id}`);
+const job = queue.create('push_notification_code', jobData);
+    job.save((err) => {
+        if (!err){ console.log(`Notification job created: ${queue.id.slice(14, -1)}`);
+    }
     });
 
-push_notification_code.on('complete', () => console.log('Notification job completed'))
+job.on('complete', () => console.log('Notification job completed'))
     .on('fail', () => console.log('Notification job fail'))
--- 0x03-queuing_system_in_js/6-job_processor.js
+++ 0x03-queuing_system_in_js/6-job_processor.js
@@ -3,11 +3,11 @@ const process = new_kue.createQueue()
 
 const sendNotification = (phoneNumber, message) => {
     console.log(`Sending notification to ${phoneNumber}, with message: ${message}`);
-} 
+}
 //The `push notification is the name of the job` 
- process.process('push notification',(job, done) => {
-    if(job) {
-            sendNotification(job.data.phoneNumber, job.data.message, done)
-           // done()
+process.process('push_notification_code', (job, done) => {
+    if (job) {
+        sendNotification(job.data.phoneNumber, job.data.message);
+        done();
     }
 });
